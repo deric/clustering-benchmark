12339f9a7b4ec1e19cfaadb78f7e239591635272
Tomas Barton <barton.tomas@gmail.com>
2015-06-09 21:19:30 +0200

    fix package

--- src/main/java/org/clueminer/data/DataLoader.java
+++ src/main/java/org/clueminer/data/DataLoader.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.clueminer.demo.data;
+package org.clueminer.data;
 
 import java.io.File;
 import java.io.FileNotFoundException;
