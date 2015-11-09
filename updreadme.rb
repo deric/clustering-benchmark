12561a0e234fedd95c4da17b04b62b0d2a2fd53a
Tomas Barton <barton.tomas@gmail.com>
2015-11-09 16:17:52 +0100

    update readme

--- updreadme.rb
+++ updreadme.rb
@@ -13,7 +13,7 @@ end
 datasets.sort.each do |data|
   base = basename(data)
 img = <<-EOS
-<a href="https://github.com/deric/clustering-benchmark/blob/master/src/main/resources/datasets/artificial/#{data}"><img src="https://github.com/deric/clustering-benchmark/blob/images/fig/artificial/#{base}.png" alt="#{base}" title="#{base}" width="250px" style="max-width: 100%;float:left;"/></a>
+<a href="https://github.com/deric/clustering-benchmark/blob/master/src/main/resources/datasets/artificial/#{data}"><img src="https://github.com/deric/clustering-benchmark/blob/images/fig/artificial/#{base.downcase}.png" alt="#{base}" title="#{base}" width="250px" style="max-width: 100%;float:left;"/></a>
 EOS
 puts img
 end
